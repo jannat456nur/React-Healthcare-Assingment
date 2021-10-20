d1c4342a3ff386b9a63d1d51f42e5a900cbf48f9
jannat456nur <jannatjenny22@gmail.com>
2021-10-19 22:43:53 -0700

    add email

--- src/hooks/useFirebase.js
+++ src/hooks/useFirebase.js
@@ -1,17 +1,72 @@
 import { useEffect, useState } from "react"
 // import { initializeAuth } from "../Firebase/firebase.init";
 import initializeAuth from "../Firebase/firebase.init";
-import { getAuth, signInWithPopup, signOut, onAuthStateChanged, GoogleAuthProvider } from "firebase/auth";
+import { getAuth, signInWithPopup, signOut, onAuthStateChanged, GoogleAuthProvider, signInWithEmailAndPassword, createUserWithEmailAndPassword, sendEmailVerification } from "firebase/auth";
 
 initializeAuth()
 
 const useFirebase = () => {
+    const [email, setEmail] = useState('')
+    const [pass, setPass] = useState();
+    const [error, setError] = useState('')
+    const [login, setlogin] = useState(false)
     const [user, setUser] = useState({});
 
     const auth = getAuth();
     const googleProvider = new GoogleAuthProvider();
 
+    // const auth = getAuth();
+    const handleEmailChange = (e) => {
+        setEmail(e.target.value)
+    }
+    const handlePassChange = (e) => {
+        setPass(e.target.value)
+    }
+    const toogleLogIn = e => {
+        setlogin(e.target.checked)
+    }
 
+    const handleRegistration = e => {
+        e.preventDefault();
+        console.log(email, pass)
+        if (pass.length < 6) {
+            setError('password should be 6 character')
+            return;
+        }
+        login ? processLogin(email, pass) : createnewuser(email, pass);
+
+
+    }
+    const processLogin = (email, pass) => {
+        signInWithEmailAndPassword(auth, email, pass)
+            .then(result => {
+                const user = result.user;
+                setError('')
+            })
+            .catch(error => {
+                setError(error.message)
+            })
+    }
+    const createnewuser = (email, pass) => {
+        createUserWithEmailAndPassword(auth, email, pass)
+            .then((result) => {
+                const user = result.user;
+                console.log(user)
+                setError('')
+                // verifyEmail()
+
+            })
+            .catch(error => {
+                setError(error.message)
+            })
+    }
+    const verify = () => {
+        const auth = getAuth();
+        sendEmailVerification(auth.currentUser)
+            .then((result) => {
+                console.log(result)
+            });
+    }
 
     const signInUsingGoogle = () => {
         signInWithPopup(auth, googleProvider)
@@ -45,7 +100,14 @@ const useFirebase = () => {
     return {
         user,
         signInUsingGoogle,
-        logOut
+        logOut,
+        verify,
+        handleRegistration,
+        toogleLogIn,
+        handleEmailChange,
+        handlePassChange, setError, login, error
+
+
     }
 }
 export default useFirebase;
--- src/pages/Signup/Signup.js
+++ src/pages/Signup/Signup.js
@@ -5,51 +5,90 @@ import useFirebase from '../../hooks/useFirebase';
 import './Signup.css'
 
 const Signup = () => {
+    const { user, signInUsingGoogle, verify,
+        handleRegistration,
+        toogleLogIn,
+        handleEmailChange,
+        handlePassChange, setError, login, error } = useFirebase()
 
-    const [email, setEmail] = useState('')
-    const [pass, setPass] = useState();
+    // const [email, setEmail] = useState('')
+    // const [pass, setPass] = useState();
 
-    const auth = getAuth();
-    const handleEmailChange = (e) => {
-        setEmail(e.target.value)
-    }
-    const handlePassChange = (e) => {
-        setPass(e.target.value)
-    }
+    // const auth = getAuth();
+    // const handleEmailChange = (e) => {
+    //     setEmail(e.target.value)
+    // }
+    // const handlePassChange = (e) => {
+    //     setPass(e.target.value)
+    // }
 
-    const handleRegistration = e => {
-        console.log(email, pass)
-        e.preventDefault();
-    }
-    const { user, signInUsingGoogle } = useFirebase()
+    // const handleRegistration = e => {
+    //     console.log(email, pass)
+    //     e.preventDefault();
+    // }
+
     return (
-        <div className="d-flex dakters-bg-danger" id="signup">
-            {/* <h1 className="m-5 text-white">Signup For Latest Updates and News</h1>
-            <input className="m-5 p-0" type="text" placeholder=" your name" />
-            <input className="m-5" type="password" name="" placeholder="password" id="" /> */}
+        // <div className="d-flex dakters-bg-danger" id="signup">
 
-            <form onSubmit={handleRegistration} className="row g-3">
-                <div className="col-auto">
 
-                    <h1 className="m-5 text-white">Signup For Latest <br /> Updates and News</h1>
+        //     <form onSubmit={handleRegistration} className="row g-3">
+        //         <div className="col-auto">
+
+        //             <h1 className="m-5 text-white">Signup For Latest <br /> Updates and News</h1>
+        //         </div>
+        //         <div className="col-auto pt-5">
+        //             <label htmlFor="inputPassword2" className="visually-hidden">Email</label>
+        //             <input onChange={handleEmailChange} type="email" className="form-control" id="inputPassword2" placeholder="email" />
+        //         </div>
+        //         <div className="col-auto pt-5">
+        //             <label htmlFor="inputPassword2" className="visually-hidden">Email</label>
+        //             <input onChange={handlePassChange} type="password" className="form-control" id="inputPassword2" placeholder="password" />
+        //         </div>
+
+        //         <div className="col-auto pt-5">
+        //             <Link><button type="button" className="btn btn-light">Register</button></Link>
+        //         </div>
+        //         <div className="col-auto pt-5">
+        //             <button onClick={signInUsingGoogle} type="button" className="btn btn-light">Google signIn</button>
+        //         </div>
+        //     </form>
+        // </div>
+
+        <div className="App">
+            <form onSubmit={handleRegistration}>
+                <h4>Please {login ? "Login" : "Register"}</h4>
+                <div class="row mb-3">
+                    <label for="inputEmail3" class="col-sm-2 col-form-label">Email</label>
+                    <div class="col-sm-10">
+                        <input onBlur={handleEmailChange} type="email" class="form-control" id="inputEmail3" />
+                    </div>
                 </div>
-                <div className="col-auto pt-5">
-                    <label htmlFor="inputPassword2" className="visually-hidden">Email</label>
-                    <input onChange={handleEmailChange} type="email" className="form-control" id="inputPassword2" placeholder="email" />
-                </div>
-                <div className="col-auto pt-5">
-                    <label htmlFor="inputPassword2" className="visually-hidden">Email</label>
-                    <input onChange={handlePassChange} type="password" className="form-control" id="inputPassword2" placeholder="password" />
+                <div class="row mb-3">
+                    <label for="inputPassword3" class="col-sm-2 col-form-label">Password</label>
+                    <div class="col-sm-10">
+                        <input onBlur={handlePassChange} type="password" class="form-control" id="inputPassword3" />
+                    </div>
                 </div>
 
-                <div className="col-auto pt-5">
-                    <Link><button type="button" className="btn btn-light">Register</button></Link>
+                <div class="row mb-3">
+                    <div class="col-sm-10 offset-sm-2">
+                        <div class="form-check">
+                            <input onChange={toogleLogIn} class="form-check-input" type="checkbox" id="gridCheck1" />
+                            <label class="form-check-label" for="gridCheck1">
+                                Already Registered?
+                            </label>
+                        </div>
+                    </div>
                 </div>
-                <div className="col-auto pt-5">
-                    <button onClick={signInUsingGoogle} type="button" className="btn btn-light">Google signIn</button>
-                </div>
+                <div className="row mb-3 text-danger">{error}</div>
+                <button type="submit" class="btn btn-primary">{login ? "Login" : "Registration"}</button>
             </form>
+
+            <div className="col-auto pt-5">
+                <button onClick={signInUsingGoogle} type="button" className="btn btn-light">Google signIn</button>
+            </div>
         </div>
+
     );
 };
 
